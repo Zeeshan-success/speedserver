e979e9715b4c4fdab6427b94f33ff44dcfa6c27b
Zeeshan <zeeshanmalik.1011@gmail.com>
2025-07-05 14:47:16 +0500

    fix cors error

--- index.js
+++ index.js
@@ -18,7 +18,11 @@ process.on('unhandledRejection', (reason, promise) => {
 });
 
 // CORS configuration - Allow all origins for development
-app.use(cors());
+app.use(cors({
+  origin: 'http://localhost:3000', // Or '*' to allow all origins (for dev only)
+  methods: ['GET', 'POST'],
+  credentials: true
+}));
 
 // Middleware for parsing JSON and raw data
 app.use(express.json({ limit: '500mb' }));
